cb72932300d8e14048cbd509252d1d380cc99e2f
Bunty Sharma <110980254+Bunty3545@users.noreply.github.com>
2026-09-23 01:06:32 +0530

    Time: 4 ms (53.37%), Space: 14.9 MB (44.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
-public:
+    public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-
-        for(int i = 0; i < nums.size(); i++) {
+        unordered_map<int, int> num_map;
+        for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
-
-            if(mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
-            }
-
-            mp[nums[i]] = i;
-        }
-
-        return {};
-    }
-};
+            if (num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
+                }
+                num_map[nums[i]] = i;
+                }
+                return {};
+                }
+                };                  
+            
